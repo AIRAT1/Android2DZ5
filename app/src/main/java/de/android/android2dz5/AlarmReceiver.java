49e1b687d39248d0d599885cd0a7fb39eed4fa91
Ayrat <ayrat1@mail.ru>
2016-03-18 17:52:16 +0300

    complete with alarm on and off

--- app/src/main/java/de/android/android2dz5/AlarmReceiver.java
+++ app/src/main/java/de/android/android2dz5/AlarmReceiver.java
@@ -3,12 +3,10 @@ package de.android.android2dz5;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d("LOG", "onReceive");
         String getExtraString = intent.getExtras().getString(MainActivity.EXTRA);
         Intent serviceIntent = new Intent(context, RingtonePlayingService.class);
         serviceIntent.putExtra(MainActivity.EXTRA, getExtraString);
--- app/src/main/java/de/android/android2dz5/RingtonePlayingService.java
+++ app/src/main/java/de/android/android2dz5/RingtonePlayingService.java
@@ -4,11 +4,11 @@ import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.IBinder;
-import android.util.Log;
 
 public class RingtonePlayingService extends Service {
     private MediaPlayer mediaPlayer;
-    private int startID;
+    private int startId;
+    private boolean isRunning;
     public RingtonePlayingService() {
     }
 
@@ -27,14 +27,34 @@ public class RingtonePlayingService extends Service {
                 startId = 0;
                 break;
         }
-        mediaPlayer = MediaPlayer.create(this, R.raw.violin_stirling);
-        Log.d("LOG", "onStartCommand");
-        mediaPlayer.start();
+        // music off and user pressed on
+        if (!this.isRunning && startId == 1) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.violin_stirling);
+            mediaPlayer.start();
+            this.isRunning = true;
+            this.startId = 0;
+        }
+        // music on and user press off
+        else if (this.isRunning && startId == 0) {
+            mediaPlayer.stop();
+            mediaPlayer.reset();
+            this.isRunning = false;
+            this.startId = 0;
+        }
+        // music off and user press off
+        else if (!this.isRunning && startId == 0) {
+            this.isRunning = false;
+            this.startId = 0;
+        }
+        // music on and user press on
+        else if (this.isRunning && startId == 1) {
+            this.isRunning = true;
+            this.startId = 1;
+        }
+        // just to catch the add event
+        else {
 
-
-
-
-
+        }
         return START_NOT_STICKY;
     }
 
@@ -42,5 +62,10 @@ public class RingtonePlayingService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 }
